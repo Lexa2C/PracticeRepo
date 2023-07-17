43adaab8936e792925488a470c84122182891132
LeksAleksa <adzarkovic@gmail.com>
2023-07-17 17:49:50 +0200

    This is first commit of some C code {Garbage code}

---
+++ TestCode.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdint.h>
+
+// This is new and first test code written i C lang
+
+uint8_t var_a = 0;
+char b = 1;
+
+void main(void)
+{
+    printf("This is example\n");
+    printf("\nVal a = %d\nVal b = %d", var_a, b);
+
+}
